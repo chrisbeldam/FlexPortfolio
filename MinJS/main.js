afcf1035685dcd073871cc16316c98360ce4ff28
Chris Beldam <chrisgbeldam@gmail.com>
2018-04-10 20:37:40 +0100

    Dev Icons

--- MinJS/main.js
+++ MinJS/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".item-info").hide(),$(".item").mouseenter(function(){$(this).find(".item-info").fadeIn(100),$(this).css("cursor","pointer")}),$(".item").mouseleave(function(){$(this).find(".item-info").fadeOut(100)})});
+$(document).ready(function(){$(".item-info").hide(),$(".item1").mouseenter(function(){$(this).find(".item-info1").fadeIn(100),$(this).css("cursor","pointer")}),$(".item1").mouseleave(function(){$(this).find(".item-info1").fadeOut(100)})});
